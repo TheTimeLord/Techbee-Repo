8bb706a3a14b6ea7ab4acb219f3b4f862248b622
TheTimeLord <brianandrus2@gmail.com>
2021-04-08 15:19:24 -0700

    Finished and Tested Vowels.

--- src/StringManipulation/Testing.java
+++ src/StringManipulation/Testing.java
@@ -3,5 +3,8 @@ package StringManipulation;
 public class Testing {
 	public static void main(String[] args) {
 		
+		String str = "Hello There!";
+		
+		System.out.println(Vowels.countVowels(str));
 	}
 }
