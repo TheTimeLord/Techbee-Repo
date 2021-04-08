84146806d8dd4c84921dc87a52e62692301f9167
TheTimeLord <brianandrus2@gmail.com>
2021-04-08 15:35:36 -0700

    Completed Palindrome class and testing

--- src/StringManipulation/Palindrome.java
+++ src/StringManipulation/Palindrome.java
@@ -1,5 +1,12 @@
 package StringManipulation;
+import java.lang.StringBuilder;
 
 public class Palindrome {
-	
+	public static boolean isPalindrome(String str) {
+		boolean palindrome = false;
+		String reverse = new StringBuilder(str).reverse().toString();
+		if(str.equals(reverse))
+			palindrome = true;
+		return palindrome;
+	}
 }
--- src/StringManipulation/Testing.java
+++ src/StringManipulation/Testing.java
@@ -3,8 +3,15 @@ package StringManipulation;
 public class Testing {
 	public static void main(String[] args) {
 		
-		String str = "Hello There!";
+		String str = "Hello There";
+		String pal = "racecar";
 		
+		System.out.println(str);
 		System.out.println(Vowels.countVowels(str));
+		System.out.println(Palindrome.isPalindrome(str) + "\n");
+		
+		System.out.println(pal);
+		System.out.println(Vowels.countVowels(pal));
+		System.out.println(Palindrome.isPalindrome(pal));
 	}
 }
