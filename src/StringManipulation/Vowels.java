6f758860a1af9df8a259a04a80b96fe60f3a4b3d
TheTimeLord <brianandrus2@gmail.com>
2021-04-08 15:12:39 -0700

    Added Vowels counter

--- src/StringManipulation/Vowels.java
+++ src/StringManipulation/Vowels.java
@@ -1,5 +1,13 @@
 package StringManipulation;
 
 public class Vowels {
-
+	
+	public static int countVowels(String str) {
+		int numVowels = 0;
+		for(int i=0; i<str.length(); i++) {
+			if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u')
+				numVowels++;
+		}
+		return numVowels;
+	}
 }
