3e4e0ed6e213638bb08b0b28f99f333884ef4003
TheTimeLord <brianandrus2@gmail.com>
2021-04-08 15:47:39 -0700

    testing changes

--- src/StringManipulation/Testing.java
+++ src/StringManipulation/Testing.java
@@ -8,10 +8,15 @@ public class Testing {
 		
 		System.out.println(str);
 		System.out.println(Vowels.countVowels(str));
-		System.out.println(Palindrome.isPalindrome(str) + "\n");
+		System.out.println(Palindrome.isPalindrome(str));
+		System.out.println(Words.countWords(str));
+		
+		System.out.println();
 		
 		System.out.println(pal);
 		System.out.println(Vowels.countVowels(pal));
 		System.out.println(Palindrome.isPalindrome(pal));
+		System.out.println(Words.countWords(pal));
+		
 	}
 }
