b5a24d07f8d4ef3018cea331842e2f54d62df808
TheTimeLord <brianandrus2@gmail.com>
2021-04-08 15:43:17 -0700

    Added Word counting functionality.

--- src/StringManipulation/Words.java
+++ src/StringManipulation/Words.java
@@ -1,5 +1,14 @@
 package StringManipulation;
 
 public class Words {
-
+	public static int countWords(String str) {
+		int numWords = 0;
+		if(str.length() > 0)
+			numWords++;
+		for(int i=0; i<str.length(); i++) {
+			if(str.charAt(i) == ' ')
+				numWords++;
+		}
+		return numWords;
+	}
 }
