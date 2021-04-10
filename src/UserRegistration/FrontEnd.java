107ba7414827ab257d86a4e7a2973b90d6f21d44
TheTimeLord <brianandrus2@gmail.com>
2021-04-10 15:29:53 -0700

    Added basic properties and functionalities to user profile. front end can add and display all users.

--- src/UserRegistration/FrontEnd.java
+++ src/UserRegistration/FrontEnd.java
@@ -1,5 +1,51 @@
 package UserRegistration;
 
-public class FrontEnd {
+import java.util.Scanner;
 
+public class FrontEnd {
+	public static void main(String[] args) {
+		
+		boolean keepGoing;
+		Scanner scan = new Scanner(System.in);
+		UserProfileDAO database = new UserProfileDAO();
+		
+		do {
+			keepGoing = false;
+	
+			// Add user
+			System.out.print("Add new user? (y/n): ");
+			if(scan.next().charAt(0) == 'y') {
+				UserProfile user = new UserProfile();
+
+				// TODO: handle input mismatch exceptions one at a time
+				System.out.print("Enter name: ");
+				user.setName(scan.next());
+				System.out.print("Enter Age: ");
+				user.setAge(scan.nextInt());
+				System.out.print("Enter email: ");
+				user.setEmailAddress(scan.next());
+				System.out.print("Enter phone number: ");
+				user.setPhoneNumber(scan.nextInt());
+				System.out.print("Enter zipcode: ");
+				user.setZipcode(scan.nextInt());
+				System.out.print("Enter SSN: ");
+				user.setSsnNumber(scan.nextInt());
+
+				database.addUser(user);
+				
+			}
+			
+			// print all users
+			System.out.print("Would you like to print all users? (y/n): ");
+			if(scan.next().charAt(0) == 'y')
+				database.displayUsers();
+			
+			
+			// Ask to continue
+			System.out.print("Would you like to continue? (y/n): ");
+			if(scan.next().charAt(0) == 'y')
+				keepGoing = true;
+		} while(keepGoing);
+		
+	}
 }
--- src/UserRegistration/UserProfileDAO.java
+++ src/UserRegistration/UserProfileDAO.java
@@ -1,5 +1,52 @@
 package UserRegistration;
 
-public class UserProfileDAO {
+import java.util.ArrayList;
 
+public class UserProfileDAO {
+	ArrayList<UserProfile> data = new ArrayList();
+	
+	// Create User
+	public void addUser(UserProfile user) {
+		data.add(user);
+	}
+	
+	// Read Users
+	public void displayUsers() {
+		for(int i=0; i<data.size(); i++) {
+			System.out.println("Name: " + data.get(i).getName());
+			System.out.println("Age: " + data.get(i).getAge());
+			System.out.println("Email: "  + data.get(i).getEmailAddress());
+			System.out.println("Phone: " + data.get(i).getPhoneNumber());
+			System.out.println("Zip: " + data.get(i).getZipcode());
+			System.out.println("SSN: " + data.get(i).getSsnNumber());
+			
+		}
+	}
+	public void displayUser(UserProfile user) {
+		System.out.println("Name: " + user.getName());
+		System.out.println("Age: " + user.getAge());
+		System.out.println("Email: "  + user.getEmailAddress());
+		System.out.println("Phone: " + user.getPhoneNumber());
+		System.out.println("Zip: " + user.getZipcode());
+		System.out.println("SSN: " + user.getSsnNumber());
+	}
+	
+	// Update User
+	public void updateUser(UserProfile user, String name, int age, String email, int phone, int zip, int ssn) {
+		user.setName(name);
+		user.setAge(age);
+		user.setEmailAddress(email);
+		user.setPhoneNumber(phone);
+		user.setZipcode(zip);
+		user.setSsnNumber(ssn);
+	}
+	
+	// Delete User
+	public void deleteUser(UserProfile user) {
+		for(int i=0; i<data.size(); i++) {
+			if(user.equals(data.get(i))) {
+				data.remove(i);
+			}
+		}
+	}
 }
